44abb66d2715db886a9f9af5ca2c0e38fca51c41
Rohen Chawla <rrchawla@usc.edu>
2022-06-20 00:49:51 -0500

    pushing week2

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import profile_pic_name from "../assets/profile_pic_name.JPG";
+  
 export default class About extends Component {
   render() {
     return (
-      <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+        <div>
+            <div class="split left">
+                <div className="centered">
+                <img 
+                    className="profile_image"
+                    src={profile_pic_name}
+                    alt="Profile Pic"
+                    ></img>
+                </div>
             </div>
-          </div>
-        </div>
+            <div className="split right">
+                <div className="centered">
+                <div className="name_title">Rohen Chawla❗</div>
+                <div className="brief_description">
+                    I like to sleep, jumprope, and skateboard at night.
+                    <br></br>
+                    I also tend to procrastinate on important work by doing busy work instead to feel productive. 🤩
+                </div>
+                </div>
+            </div>
       </div>
-    );
+    )
   }
 }
